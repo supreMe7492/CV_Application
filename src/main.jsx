eae762d777487e34b212b961d5f19c11cf8f88b5
Supreme Raut <supremeraut94@gmail.com>
2025-01-05 12:56:01 +0545

    deploying

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import "./styles/index.css";
 import App from "./components/App.jsx";
 
